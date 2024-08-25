cf142876d324f3d597771cb117e60ebf9a6de2bd
twonezero <dnjsdudtm98@naver.com>
2024-08-25 16:11:43 +0900

    #7 - jpa auditing fields 클래스 정의 : BaseEntity
    - 공통으로 적용되는 감사로그 필드들과, `isPublic`, `isDeleted` 필드를 정의하여 추후 작성되는 엔티티 클래스에서 상속해서 사용하기

---
+++ src/main/java/com/sparta/project/delivery/common/BaseEntity.java
@@ -0,0 +1,57 @@
+package com.sparta.project.delivery.common;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Setter
+    @ColumnDefault(value = "true")
+    @Column(name = "is_public", nullable = false)
+    private Boolean isPublic;
+
+    @Setter
+    @ColumnDefault(value = "false")
+    @Column(name = "is_deleted", nullable = false)
+    private Boolean isDeleted;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(name = "created_by", length = 100, nullable = false, updatable = false)
+    private String createdBy;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    @Column(name = "updated_by", length = 100)
+    private String updatedBy;
+
+    @Setter
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Setter
+    @Column(name = "deleted_by", length = 100)
+    private String deletedBy;
+
+}
