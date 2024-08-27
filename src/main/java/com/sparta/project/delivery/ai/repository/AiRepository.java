d6c5549d728fbcc4642f8afa8efb0983149afe7a
hosu <hojune0904@gmail.com>
2024-08-27 15:36:18 +0900

    #40 - ai api 개발 ai 호출하여 답변을 받아오고 해당 답변을 뿌려주는 API를 개발하였습니다.

---
+++ src/main/java/com/sparta/project/delivery/ai/repository/AiRepository.java
@@ -0,0 +1,1 @@
+package com.sparta.project.delivery.ai.repository;import com.sparta.project.delivery.ai.Entity.Ai;import org.springframework.data.jpa.repository.JpaRepository;public interface AiRepository extends JpaRepository<Ai, String> {}
