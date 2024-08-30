dbede947fadd2bc5a8082742852dbd32fd01bea0
hosu <76641246+junghojune@users.noreply.github.com>
2024-08-30 11:55:43 +0900

    #63 ai api 문서(swagger) 작성, user 권한 , validation 체크 라인 정리

--- src/main/java/com/sparta/project/delivery/ai/Entity/Ai.java
+++ src/main/java/com/sparta/project/delivery/ai/Entity/Ai.java
@@ -1,1 +1,30 @@
-package com.sparta.project.delivery.ai.Entity;import com.sparta.project.delivery.common.BaseEntity;import com.sparta.project.delivery.user.User;import jakarta.persistence.*;import lombok.*;@NoArgsConstructor(access = AccessLevel.PROTECTED)@AllArgsConstructor(access = AccessLevel.PRIVATE)@Builder@Getter@Entity@Table(name = "p_ai")public class Ai extends BaseEntity {    @Id    @GeneratedValue(strategy = GenerationType.UUID)    @Column(name = "ai_id")    private String aiId;    @ManyToOne(fetch = FetchType.LAZY)    @JoinColumn(name = "user_id", nullable = true)    private User user;    @Column(length = 100, nullable = false)    private String question;    @Column(length = 100, nullable = false)    private String answer;}
+package com.sparta.project.delivery.ai.Entity;
+
+import com.sparta.project.delivery.common.BaseEntity;
+import com.sparta.project.delivery.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+@Entity
+@Table(name = "p_ai")
+public class Ai extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "ai_id")
+    private String aiId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = true)
+    private User user;
+
+    @Column(length = 100, nullable = false)
+    private String question;
+
+    @Column(length = 100, nullable = false)
+    private String answer;
+}
--- src/main/java/com/sparta/project/delivery/ai/dto/AiRequest.java
+++ src/main/java/com/sparta/project/delivery/ai/dto/AiRequest.java
@@ -1,1 +1,11 @@
-package com.sparta.project.delivery.ai.dto;import com.sparta.project.delivery.user.dto.UserDto;public record AiRequest(        String question) {    public AiDto toDto(UserDto dto) {        return AiDto.of(dto, question);    }}
+package com.sparta.project.delivery.ai.dto;
+
+import com.sparta.project.delivery.user.dto.UserDto;
+
+public record AiRequest(
+        String question
+) {
+    public AiDto toDto(UserDto dto) {
+        return AiDto.of(dto, question);
+    }
+}
