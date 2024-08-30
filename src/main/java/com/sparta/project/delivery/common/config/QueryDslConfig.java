3954c8f7453bd4bca922aca21663dc4a24aaf70e
twonezero <dnjsdudtm98@naver.com>
2024-08-30 12:20:12 +0900

    #43 - queryDsl 설정 파일 작성

---
+++ src/main/java/com/sparta/project/delivery/common/config/QueryDslConfig.java
@@ -0,0 +1,15 @@
+package com.sparta.project.delivery.common.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QueryDslConfig {
+
+    @Bean
+    JPAQueryFactory jpaQueryFactory(EntityManager em){
+        return new JPAQueryFactory(em);
+    }
+}
