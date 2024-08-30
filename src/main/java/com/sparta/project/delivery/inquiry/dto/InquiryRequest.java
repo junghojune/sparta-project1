01806947b7cc1949bd26fc6901c7daae6dbcf6a3
twonezero <dnjsdudtm98@naver.com>
2024-08-30 15:00:41 +0900

    #65 - Inquiry API 수정
    - 유저 권한 추가
    - 반환 타입 수정
    - 조회 로직 수정

--- src/main/java/com/sparta/project/delivery/inquiry/dto/InquiryRequest.java
+++ src/main/java/com/sparta/project/delivery/inquiry/dto/InquiryRequest.java
@@ -1,21 +1,18 @@
 package com.sparta.project.delivery.inquiry.dto;
 
-import com.sparta.project.delivery.inquiry.constant.InquiryStatus;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record InquiryRequest(
+        @NotBlank(message = "제목은 비어 있을 수 없습니다.")
+        @Size(max = 100, message = "제목은 최대 100자까지 입력할 수 있습니다.")
         String title,
+
+        @NotBlank(message = "내용은 비어 있을 수 없습니다.")
+        @Size(max = 5000, message = "내용은 최대 5000자까지 입력할 수 있습니다.")
         String content
 ) {
-
-    public InquiryDto toDto(String userEmail){
-        return InquiryDto.builder()
-                .userEmail(userEmail)
-                .title(title)
-                .content(content)
-                .status(InquiryStatus.RECEIVED)
-                .build();
-    }
-
 
     public InquiryDto toDto(){
         return InquiryDto.builder()
