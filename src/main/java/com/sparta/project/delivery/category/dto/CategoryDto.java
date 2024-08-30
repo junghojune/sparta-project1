1965ac8a83302a75aae26b3d115f118a7f351148
hosu <76641246+junghojune@users.noreply.github.com>
2024-08-30 12:36:32 +0900

    #63 category api 문서(swagger) 작성, user 권한 , validation 체크 라인 정리

--- src/main/java/com/sparta/project/delivery/category/dto/CategoryDto.java
+++ src/main/java/com/sparta/project/delivery/category/dto/CategoryDto.java
@@ -1,1 +1,50 @@
-package com.sparta.project.delivery.category.dto;import com.sparta.project.delivery.category.entity.Category;import java.time.LocalDateTime;public record CategoryDto(        String categoryId,        String name,        Boolean isPublic,        Boolean isDeleted,        LocalDateTime createdAt,        String createdBy,        LocalDateTime updatedAt,        String updatedBy,        LocalDateTime deletedAt,        String deletedBy) {    public static CategoryDto of(String name) {        return new CategoryDto(null, name, null, null, null, null, null, null, null, null);    }    public static CategoryDto of(String categoryId, String name, Boolean isPublic, Boolean isDeleted) {        return new CategoryDto(categoryId, name, isPublic, isDeleted,                null, null, null, null, null, null);    }    public static CategoryDto from(Category category) {        return new CategoryDto(                category.getCategoryId(),                category.getName(),                category.getIsPublic(),                category.getIsDeleted(),                category.getCreatedAt(),                category.getCreatedBy(),                category.getUpdatedAt(),                category.getUpdatedBy(),                category.getDeletedAt(),                category.getDeletedBy()        );    }    public Category toEntity() {        return Category.builder()                .name(name)                .build();    }}
+package com.sparta.project.delivery.category.dto;
+
+import com.sparta.project.delivery.category.entity.Category;
+
+import java.time.LocalDateTime;
+
+public record CategoryDto(
+        String categoryId,
+        String name,
+        Boolean isPublic,
+        Boolean isDeleted,
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime updatedAt,
+        String updatedBy,
+        LocalDateTime deletedAt,
+        String deletedBy
+) {
+
+    public static CategoryDto of(String name) {
+        return new CategoryDto(null, name, null, null, null, null, null, null, null, null);
+    }
+
+    public static CategoryDto of(String categoryId, String name, Boolean isPublic, Boolean isDeleted) {
+
+        return new CategoryDto(categoryId, name, isPublic, isDeleted,
+                null, null, null, null, null, null);
+    }
+
+    public static CategoryDto from(Category category) {
+        return new CategoryDto(
+                category.getCategoryId(),
+                category.getName(),
+                category.getIsPublic(),
+                category.getIsDeleted(),
+                category.getCreatedAt(),
+                category.getCreatedBy(),
+                category.getUpdatedAt(),
+                category.getUpdatedBy(),
+                category.getDeletedAt(),
+                category.getDeletedBy()
+        );
+    }
+
+    public Category toEntity() {
+        return Category.builder()
+                .name(name)
+                .build();
+    }
+}
